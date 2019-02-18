1d126836eee066e1868fad07563c97d24093c4b8
yangjun28 <yangjun28@lenovo.com>
2019-02-18 12:29:39 +0800

    merge

--- src/assets/js/nodejsOperatorAll.js
+++ src/assets/js/nodejsOperatorAll.js
@@ -27,38 +27,6 @@ function getIndexOfPathByDeep(obj, dir, curDir, deep) {
 fs.writeFileSync(path.join(__dirname, '../json/text.json'), JSON.stringify(getIndexByPath('../markdown')));
 
 console.log('程序执行完毕fsReadFileToJson。');
-// node 读取csv文件生成二位数组
-var path = require('path');
-var fs = require('fs');
-
-fs.readFile(path.join(__dirname, '../file/PCG_dbo_allCountryInfo_manualAdjustment_2.csv'), function (err, data) {
-  var table = new Array();
-  if (err) {
-    console.log(err.stack);
-    return;
-  }
-
-  ConvertToTable(data);
-});
-
-function ConvertToTable(data) {
-  data = data.toString();
-  var table = new Array();
-  var rows = new Array();
-  rows = data.split("\r\n");
-  for (var i = 0; i < rows.length; i++) {
-    table.push(rows[i].split(","));
-  }
-  fs.writeFile(path.join(__dirname, '../json/PCG_dbo_allCountryInfo_manualAdjustment_2.json'), JSON.stringify(table), (err) => {
-    if (err) {
-      console.log(err);
-    }else{
-      console.log('success');
-    }
-  });
-}
-
-console.log('程序执行完毕nodeGetCsvFile');
 // 生成同级的json数组对象
 var path = require('path');
 var fs = require('fs');
@@ -160,6 +128,38 @@ function ConvertToTable(data) {
     table.push(rows[i].split(","));
   }
   fs.writeFile(path.join(__dirname, '../json/csv.json'), JSON.stringify(table), (err) => {
+    if (err) {
+      console.log(err);
+    }else{
+      console.log('success');
+    }
+  });
+}
+
+console.log('程序执行完毕nodeGetCsvFile');
+// node 读取csv文件生成二位数组
+var path = require('path');
+var fs = require('fs');
+
+fs.readFile(path.join(__dirname, '../file/PCG_dbo_allCountryInfo_manualAdjustment_2.csv'), function (err, data) {
+  var table = new Array();
+  if (err) {
+    console.log(err.stack);
+    return;
+  }
+
+  ConvertToTable(data);
+});
+
+function ConvertToTable(data) {
+  data = data.toString();
+  var table = new Array();
+  var rows = new Array();
+  rows = data.split("\r\n");
+  for (var i = 0; i < rows.length; i++) {
+    table.push(rows[i].split(","));
+  }
+  fs.writeFile(path.join(__dirname, '../json/PCG_dbo_allCountryInfo_manualAdjustment_2.json'), JSON.stringify(table), (err) => {
     if (err) {
       console.log(err);
     }else{
