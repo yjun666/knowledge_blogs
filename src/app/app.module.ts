158a10bedff8b13b2639508063041efafc378bd9
yangjun123456 <2711301790@qq.com>
2019-10-23 14:30:23 +0800

    去掉app.module.ts中多余无效的代码

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,9 @@ import { AppComponent } from './app.component';
 import { AppUpdateService } from './app-update.service';
 import { AppService } from './app.service';
 
-// view 文件
-// import { HomeComponent, HomeService } from './view/home';
-// import { LoginComponent, LoginService } from './view/login';
-// import { MarkdownComponent, MarkdowService } from './view/markdown';
-// import { RequestTestComponent, RequestTestService } from './view/requestTest';
-// import { RxjsComponent } from './view/rxjs';
-// import { LodashComponent } from './view/lodash';
-
-
-// components
-// import { SideBarComponent, SidebarService } from './components/sidebar';
-// import { MarkdownDetailsComponent, MarkdownDetailsService } from './components/markdownDetails';
-// import { HeaderService, HeaderComponent } from './components/header';
-// import { RequestTestDetailsService, RequestTestDetailsComponent } from './components/requestTestDetails';
-// import { UploaderComponent, UploaderService } from './components/upload';
-// import { RxjsDetailsComponent } from './components/rxjsDetails';
-// import { LodashDetailsComponent } from './components/lodashDetails';
-
-// directives
-// import { AppMarkedDirective, AppTitleHoverShowDirective } from './directives';
+import { environment } from '../environments/environment';
+console.log(environment);
+const isLog = environment.isLog;
 
 // auth
 import { AuthGuard } from './shared/auth';
@@ -38,76 +21,47 @@ import { httpInterceptorProviders } from './shared/interceptor';
 import { AlertModule } from './components/alert/alert.module';
 import { ADModule } from './components/ad-banner/ad.module';
 import { LanguageTranslationModule } from '../app/shared/modules/language-translation/language-translation.module';
-import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { NZ_I18N, zh_CN } from 'ng-zorro-antd';
 // service
 import { MessageService } from './shared/services/message.service';
-import { AdService } from './components/ad-banner/ad.service';
-import { RequestCache, RequestCacheWithMap } from './shared/services/request-cache.service';
-
-const modules = [
-  AlertModule,
-  HttpClientJsonpModule,
-  ADModule,
-  LanguageTranslationModule,
-  NgZorroAntdModule
-];
-
-const components = [
-  // HomeComponent,
-  // SideBarComponent,
-  // MarkdownDetailsComponent,
-  // LoginComponent,
-  // HeaderComponent,
-  // MarkdownComponent,
-  // RequestTestDetailsComponent,
-  // RequestTestComponent,
-  // UploaderComponent,
-  // RxjsComponent,
-  // RxjsDetailsComponent,
-  // LodashComponent,
-  // LodashDetailsComponent
-];
-const directives = [
-  // AppMarkedDirective,
-  // AppTitleHoverShowDirective
-];
-
+import { RequestCacheWithMap } from './shared/services/request-cache.service';
 import { LoginService } from './shared/services/login.service';
 import { LoggerService } from './shared/services/logger.service';
 import { UploaderService } from './shared/services/uploader.service';
 import { SelectivePreloadingStrategyService } from './shared/services/selective-preloading-strategy.service';
 
+const modules = [
+  AlertModule,
+  HttpClientJsonpModule,
+  ADModule,
+  LanguageTranslationModule
+];
+
+const components = [];
+const directives = [];
+
+
 const services = [
   AppUpdateService,
   AppService,
-  // HomeService,
-  // SidebarService,
-  // MarkdownDetailsService,
   LoginService,
   AuthGuard,
-  // HeaderService,
-  // MarkdowService,
   httpInterceptorProviders,
   MessageService,
   RequestCacheWithMap,
-  // RequestTestDetailsService,
-  // RequestTestService,
   UploaderService,
   SelectivePreloadingStrategyService,
-  // AdService,
   {
     provide: LoggerService,
     // 输入参数控制是否开启打印
     useFactory: (selectivePreloadingStrategyService) => {
-      return new LoggerService(true, selectivePreloadingStrategyService); // 使用useFactory 给服务添加参数
+      return new LoggerService(isLog, selectivePreloadingStrategyService); // 使用useFactory 给服务添加参数
     },
     deps: [SelectivePreloadingStrategyService] // deps 中参数为传入useFactory方法的实参,如果该服务需要其他的服务依赖，那么从此处可以注入
   },
   { provide: NZ_I18N, useValue: zh_CN }
 ];
-import { environment } from '../environments/environment';
-console.log(environment);
-const isLog = environment.isLog;
+
 
 @NgModule({
   declarations: [
