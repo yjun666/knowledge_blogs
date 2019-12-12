4d6028de6cc72651b3280f24d1e9b3496428d85d
yangjun123456 <2711301790@qq.com>
2019-12-12 10:29:34 +0800

    修改终端常用命令文件；重新生成json文件

--- src/app/shared/interceptor/loggingInterceptor.service.ts
+++ src/app/shared/interceptor/loggingInterceptor.service.ts
@@ -16,18 +16,30 @@ export class LoggingInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         const started = Date.now();
         let ok: string;
+        let res: any;
+        let authReq = req;
 
+        let success = false;
         //     // extend server response observable with logging
         return next.handle(req)
             .pipe(
                 tap(
-                    // Succeeds when there is a response; ignore other events
-                    event => ok = event instanceof HttpResponse ? 'succeeded' : '',
-                    // Operation failed; error is an HttpErrorResponse
-                    error => ok = 'failed'
+                    (event) => {
+                        // console.log(event);
+                        success = true;
+                        ok = 'success';
+                        return res = event instanceof HttpResponse ? event : event;
+                    },
+                    (error) => {
+                        // console.log(error);
+                        success = false;
+                        ok = '失败';
+                        return res = error;
+                    }
                 ),
                 // Log when response observable either completes or errors
                 finalize(() => {
+                    console.log(success, res, res.url);
                     const elapsed = Date.now() - started;
                     const msg = `${req.method} "${req.urlWithParams}" ${ok} in ${elapsed} ms.`;
                     this.messenger.push(msg);
