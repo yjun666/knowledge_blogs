8cb50be848988803fde4bbfabb3d35efd029703b
yangjun123456 <2711301790@qq.com>
2020-01-07 13:55:41 +0800

    注入拦截器服务

--- src/app/components/requestTestDetails/requestTestDetails/requestTestDetails.module.ts
+++ src/app/components/requestTestDetails/requestTestDetails/requestTestDetails.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RequestTestDetailsComponent } from './requestTestDetails.component';
+import { httpInterceptorProviders } from '../../../shared/interceptor';
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [RequestTestDetailsComponent],
-    exports: [RequestTestDetailsComponent],
-    bootstrap: [RequestTestDetailsComponent]
+  imports: [CommonModule],
+  declarations: [RequestTestDetailsComponent],
+  providers: [httpInterceptorProviders],
+  exports: [RequestTestDetailsComponent],
+  bootstrap: [RequestTestDetailsComponent]
 })
 export class RequestTestDetailsModule { }
