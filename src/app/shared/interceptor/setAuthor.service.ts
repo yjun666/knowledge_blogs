020fb9288fb84278bd51d781d24d68ed9bde834c
yangjun123456 <2711301790@qq.com>
2020-01-16 17:58:16 +0800

    调用接口时判断cookie里token是否过期，如果过期重新登陆

--- src/app/shared/interceptor/setAuthor.service.ts
+++ src/app/shared/interceptor/setAuthor.service.ts
@@ -3,21 +3,30 @@ import { Injectable } from '@angular/core';
 import {
   HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
 } from '@angular/common/http';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
-import { Observable } from 'rxjs';
-import { getToken } from '../utils/auth';
+import { Observable, of } from 'rxjs';
+import { getToken, isLogin } from '../utils/auth';
 
 const filterNoHeadersUrlArr = ['http://10.110.147.33:8015/oauth/rest_token'];
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class SetAuthorInterceptorService implements HttpInterceptor {
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
-    console.log(req, next);
     let authReq = req;
     // 排除设置Authorization等属性的接口，可能这类接口不允许设置这类字段
     if (!filterNoHeadersUrlArr.includes(req.url)) {
+      {
+        // 登陆失效，跳转到登陆页面
+        if (!isLogin()) {
+          this.router.navigate(['/login']);
+          return undefined;
+        }
+      }
       authReq = req.clone({
         headers: req.headers.set('Authorization', `Bearer ${getToken()}`)
       });
