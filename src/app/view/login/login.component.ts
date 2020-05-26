3f459a297424f21b8378953016869acd30b779f6
yangjun28 <yangjun28@lenovo.com>
2020-05-26 13:48:10 +0800

    修改

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GetJsonService } from '../../services/getJson.service';
 import { setToken, setUserName, setPassword, getUserName, getPassword } from '../../utils/auth';
 import Cookies from 'js-cookie';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,35 +12,39 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   constructor(
     private getJson: GetJsonService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
   ngOnInit() { }
 
   login(param) {
-    // if ((param.accountNumber).indexOf('') === -1) {
-    //   console.error('账号错误,请重新输入');
-    //   alert('账号错误,请重新输入');
-    //   // return false;
-    // } else if (param.password !== '') {
-    //   console.error('密码错误,请重新输入');
-    //   alert('密码错误,请重新输入');
-    //   // return false;
-    // }
-    // this.getJson.login({
-    //   client_id: 'dps-china',
-    //   client_secret: 'dps-china',
-    //   grant_type: 'password',
-    //   scope: 'read',
-    //   username: 'wangqian',
-    //   password: 'wangqian'
-    // }).subscribe((data: any) => {
-    setUserName('username');
-    setPassword('password');
-    // setToken(data.access_token);
-    setToken('8653a72a-c53a-4c96-bc49-092c9bd0cdd3');
+    if (param.accountNumber === '') {
+      console.error('账号错误,请重新输入');
+      alert('账号错误,请重新输入');
+      // return false;
+    } else if (param.password === '') {
+      console.error('密码错误,请重新输入');
+      alert('密码错误,请重新输入');
+      // return false;
+    }
+    this.getJson.login({
+      client_id: 'dps-china',
+      client_secret: 'dps-china',
+      grant_type: 'password',
+      scope: 'read',
+      username: 'test',
+      password: 'test'
+    }).subscribe((data: any) => {
+      setUserName('test');
+      setPassword('test');
+      setToken(data.access_token);
+      // setToken('8653a72a-c53a-4c96-bc49-092c9bd0cdd3');
+      console.log(data);
 
-    // 路由传参如果是传一个值需在路由配置中home后边添加"/:id",如果添加多个则不能加-----传参作为测试使用
-    this.router.navigate(['/home/markdown', { a: 'asdfasdf', b: '123123', c: 'asdf' }]);
-    // });
+      // 路由传参如果是传一个值需在路由配置中home后边添加"/:id",如果添加多个则不能加-----传参作为测试使用
+      this.router.navigate(['/home/markdown', { a: 'asdfasdf', b: '123123', c: 'asdf' }]);
+    }, (err) => {
+      console.log(err);
+    });
   }
 }
