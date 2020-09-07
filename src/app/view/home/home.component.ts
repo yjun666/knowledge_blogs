c1d1e173648c9f6a5c9ffbe3ac563d72f8592289
yangjun123456 <2711301790@qq.com>
2020-09-07 12:53:25 +0800

    home 添加接收路由参数

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router, ActivatedRoute, ParamMap, RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './routerAnimation';
 import { RouterService } from '../../services/router.service';
 import { GetJsonService } from '../../services/getJson.service';
+import { UpdataSubjectService } from '../../services/subject.service';
 import { SelectivePreloadingStrategyService } from '../../services/selective-preloading-strategy.service';
 
 
@@ -16,16 +17,21 @@ import { SelectivePreloadingStrategyService } from '../../services/selective-pre
   ]
 })
 export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('routerOutlet') routerOutlet: RouterOutlet;
   modules: string[] = [];
   constructor(
     public routerService: RouterService,
     public preloadStrategy: SelectivePreloadingStrategyService,
     private route: ActivatedRoute,
     private getJson: GetJsonService,
+    private updataSubjectService: UpdataSubjectService,
     private http: HttpClient,
     private router: Router,
   ) {
     this.modules = this.preloadStrategy.preloadedModules; // 赋值当前所有预加载的路由，可打印到页面查看当前所有预加载的路由
+    this.updataSubjectService.routerData.subscribe(data => {
+      console.log(data);
+    })
   }
 
   ngOnInit() {
@@ -34,7 +40,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     });
   }
 
-  ngAfterViewInit(): void { }
+  ngAfterViewInit(): void {}
 
   getAnimationData(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation || '';
